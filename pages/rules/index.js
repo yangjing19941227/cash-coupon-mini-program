0e57137dfe185329c62b85a7d6a6e8a36ef47312
yangjing19941227 <wuyuyangjing@126.com>
2026-06-17 19:41:15 +0800

    docs: add rules page and preview instructions

--- pages/rules/index.js
+++ pages/rules/index.js
@@ -1,1 +1,47 @@
-Page({});
+const LOTTERY_TAB_URL = '/pages/lottery/index';
+const HOME_TAB_URL = '/pages/home/index';
+
+function canUseWxApi(apiName) {
+  return typeof wx !== 'undefined' && wx && typeof wx[apiName] === 'function';
+}
+
+Page({
+  goBack() {
+    if (canUseWxApi('navigateBack')) {
+      wx.navigateBack({
+        delta: 1,
+        fail: () => {
+          this.goLotteryTab();
+        },
+      });
+      return;
+    }
+
+    this.goLotteryTab();
+  },
+
+  goLotteryTab() {
+    if (canUseWxApi('switchTab')) {
+      wx.switchTab({
+        url: LOTTERY_TAB_URL,
+        fail: () => {
+          this.goHomeTab();
+        },
+      });
+      return;
+    }
+
+    this.goHomeTab();
+  },
+
+  goHomeTab() {
+    if (canUseWxApi('switchTab')) {
+      wx.switchTab({ url: HOME_TAB_URL });
+      return;
+    }
+
+    if (canUseWxApi('reLaunch')) {
+      wx.reLaunch({ url: HOME_TAB_URL });
+    }
+  },
+});
